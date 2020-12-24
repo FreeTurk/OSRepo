c4d785f18d6dd231ffd765e3f56589f39142d041
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2020-12-24 14:43:23 +0300

    Completely made a new OSR

--- main.py
+++ main.py
@@ -1,77 +1,44 @@
-print("Checking for updates...")
-
-
-print("                                                                                                                  ")
-print("                                                                                                                  ")
-print("     OOOOOOOOO        SSSSSSSSSSSSSSS                                                                             ")
-print("   OO:::::::::OO    SS:::::::::::::::S                                                                            ")
-print(" OO:::::::::::::OO S:::::SSSSSS::::::S                                                                            ")
-print("O:::::::OOO:::::::OS:::::S     SSSSSSS                                                                            ")
-print("O::::::O   O::::::OS:::::S           rrrrr   rrrrrrrrr       eeeeeeeeeeee    ppppp   ppppppppp      ooooooooooo   ")
-print("O:::::O     O:::::OS:::::S           r::::rrr:::::::::r    ee::::::::::::ee  p::::ppp:::::::::p   oo:::::::::::oo ")
-print("O:::::O     O:::::O S::::SSSS        r:::::::::::::::::r  e::::::eeeee:::::eep:::::::::::::::::p o:::::::::::::::o")
-print("O:::::O     O:::::O  SS::::::SSSSS   rr::::::rrrrr::::::re::::::e     e:::::epp::::::ppppp::::::po:::::ooooo:::::o")
-print("O:::::O     O:::::O    SSS::::::::SS  r:::::r     r:::::re:::::::eeeee::::::e p:::::p     p:::::po::::o     o::::o")
-print("O:::::O     O:::::O       SSSSSS::::S r:::::r     rrrrrrre:::::::::::::::::e  p:::::p     p:::::po::::o     o::::o")
-print("O:::::O     O:::::O            S:::::Sr:::::r            e::::::eeeeeeeeeee   p:::::p     p:::::po::::o     o::::o")
-print("O::::::O   O::::::O            S:::::Sr:::::r            e:::::::e            p:::::p    p::::::po::::o     o::::o")
-print("O:::::::OOO:::::::OSSSSSSS     S:::::Sr:::::r            e::::::::e           p:::::ppppp:::::::po:::::ooooo:::::o")
-print(" OO:::::::::::::OO S::::::SSSSSS:::::Sr:::::r             e::::::::eeeeeeee   p::::::::::::::::p o:::::::::::::::o")
-print("   OO:::::::::OO   S:::::::::::::::SS r:::::r              ee:::::::::::::e   p::::::::::::::pp   oo:::::::::::oo ")
-print("     OOOOOOOOO      SSSSSSSSSSSSSSS   rrrrrrr                eeeeeeeeeeeeee   p::::::pppppppp       ooooooooooo   ")
-print("                                                                              p:::::p                             ")
-print("                                                                              p:::::p                             ")
-print("                                                                             p:::::::p                            ")
-print("                                                                             p:::::::p                            ")
-print("                                                                             p:::::::p                            ")
-print("                                                                             ppppppppp                            ")
-print("                                                                                                                  ")
-
-
-
-print("WELCOME TO THE BIGGEST REPO OF OPERATING SYSTEMS!")
-print("HERE, YOU WILL FIND A GREAT SELECTION OF OPERATING SYSTEMS, ALL HAND PICKED!")
-print("This is a dumb project, made by the priceless efforts of FreeTurk")
-print("")
-
-print("Throughout this script, you will be presented with a series of options.")
-print("Select accordingly, you have to know the category if you want the OS.")
-print("")
-
-print("Now, please select the type of your OS from the options of (Just write the number): ")
-print("1. *nix")
-print("2. WindowsNT")
-print("3. WindowsDOS")
-print("4. Apple")
-print("5. Legendary/Others")
-os_type = input()
-
-if os_type == "2":
-    print("PLease Wait...")
-    import winnt
-
-elif os_type == "3":
-    print("Please Wait...")
-    import windos
-
-elif os_type == "5":
-    print("Please Wait...")
-    import others
-
-
-elif os_type == "1":
-    print("Select from below options")
-    print("Linux")
-    print("Unix")
-    print("BSD")
-    nix_type = input()
-    if nix_type == "Linux":
-        import linux
-    elif nix_type == "Unix":
-        import unix
-    elif nix_type == "BSD":
-        import bsd
-
-elif os_type == "4":
-    print("Please Wait...")
-    import apple
+all_oss = []
+class OSobj:
+    def __init__(self , name , repo_item, group) :
+        self.repo_item = repo_item
+        self.name = name
+        self.group = group
+        #if group != None:
+        #    self.link = repo[group][name]["link"]
+        #else:
+        #    self.link = repo[name]["link"]
+def createosobject(name, group=None):
+    if group == None :
+        path = repo[name]
+        for os in path:
+            if os!="TYPE":
+                print("  os:", os)
+    if group != None :
+        path = repo[group][name]
+        for os in path:
+            if os!="TYPE" and name!="TYPE":
+                print("  ", name, ">",os)
+    for os in path:
+        if os != "TYPE" and name != "TYPE" :
+            current_obj = OSobj(os, group, name)
+            all_oss.append(current_obj)
+import yaml
+with open("osrepo.yaml", "r") as stream:
+    loaded_data = yaml.safe_load(stream)
+repo = loaded_data["os"]
+for index in repo:
+    repo_item = repo[index]
+    print(index)
+    type = repo_item["TYPE"]
+    if type == "os_list":
+         createosobject(index)
+    elif type == "os_group":
+        os_list = repo_item
+        for name in os_list:
+            createosobject(name, index)
+for x in all_oss:
+    if x.group != None:
+        print(x.repo_item, ">", x.group, ">", x.name)
+    else:
+        print(x.name)
--- windos.py
+++
@@ -1,2 +0,0 @@
-print("WIP area, please come back in full release!")
-input('Press ENTER to exit')
