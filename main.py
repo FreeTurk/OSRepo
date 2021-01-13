fd132c66879a989a556a6b8143d33cc2dfd95499
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-01-13 16:06:52 +0300

    optimized code, improved some functions

--- main.py
+++ main.py
@@ -1,43 +1,31 @@
-import yaml
+import yaml, requests, os, webbrowser, string, re, platform, tempfile, shutil, time
 from os import system
-import requests
 from time import sleep as delay
-import os
-import webbrowser
-import re
-import string
 from difflib import SequenceMatcher as matcher
-import re
-import platform
 
-def tag(meta, path):
-    warn = meta["warn"]
+temp =                  lambda path : shutil.copy2(path , os.path.join(tempfile.gettempdir() , "osrepo.tmp.yaml"))
+download_request  =     lambda filename : open('.\{}'.format(filename) , 'wb').write(requests.get("https://raw.githubusercontent.com/FreeTurk/OSRepo/stable/{}".format(filename) , allow_redirects=True).content)
 
-
-    return tags
-
-def initialize():
+def initialize() :
     if not os.path.isfile(".\osrepo.yaml") :
         open("osrepo.yaml" , 'w+')
-    r = requests.get("https://raw.githubusercontent.com/FreeTurk/OSRepo/stable/osrepo.yaml",allow_redirects=True)
-    open('.\osrepo.yaml' , 'wb').write(r.content)
+    download_request("osrepo.yaml")
     with open("osrepo.yaml" , "r") as stream :
         loaded_data = yaml.safe_load(stream)
     repo = loaded_data["os"]
     meta = loaded_data["meta"]
-    return repo, meta
+    return repo , meta
 
-def download(link):
+def download(link) :
     clear()
     confirm = input("Do you want to download this OS? (y/n): ")
-    if confirm.upper() == "Y":
-        webbrowser.open(link)
-    elif confirm.upper() == "N":
+    if confirm.upper() == "Y" : webbrowser.open(link)
+    elif confirm.upper() == "N" :
         clear()
         print("download cancelled.")
         delay(.75)
         walk(repo)
-    else:
+    else :
         clear()
         print("invalid")
         delay(.5)
@@ -45,108 +33,160 @@ def download(link):
         download(link)
     clear()
 
-def index(dct, key='link'):
-    if key in dct:
+def index(dct , key='link') :
+    if key in dct :
         yield ""
-    for k, v in dct.items():
-        if isinstance(v, dict):
-            for s in index(v, key):
+    for k , v in dct.items() :
+        if isinstance(v , dict) :
+            for s in index(v , key) :
                 yield f" > {k}{s}"
 
-def walk(current_dir):
-    items= []
-    for item in current_dir:
+def walk(current_dir) :
+    items = []
+    for item in current_dir :
         items.append(item)
-        if item == "link":
+        if item == "link" :
             download(current_dir["link"])
-        else:
+        else :
             print(item)
     bruh = input("\n>>")
-    if bruh in items:
+    if bruh in items :
         clear()
         walk(current_dir[bruh])
-    elif bruh.lower() == "main" or bruh.lower() == "back":
+    elif bruh.lower() == "main" or bruh.lower() == "back" :
         clear()
         walk(repo)
-    else:
+    else :
         clear()
         print("invaild.")
         delay(.5)
         clear()
         walk(current_dir)
 
-def removeduplicate(list):
-    new=[]
-    for i in list:
-        if i not in new:
+def removeduplicate(list) :
+    new = []
+    for i in list :
+        if i not in new :
             new.append(i)
     return new
 
-def search(os_list, keywords_input, success_treshold = 0.67):
-    #initial definitions
+def search(os_list , keywords_input , success_treshold=0.67) :
+    start_time = time.time()
+    # initial definitions
     filtered_input = []
     passed_filter = []
 
-    #filter the input
-    keywords = keywords_input[len("search ".lower()):].split(",")
-    for keyword in keywords:
+    # filter the input
+    keywords = keywords_input[len("search ".lower()) :].split(",")
+    for keyword in keywords :
         proper_keyword = keyword.strip("\'").strip("\"").strip(" ")
         filtered_input.append(proper_keyword)
-    #print(filtered_input, "\n\n")
+    # print(filtered_input, "\n\n")
 
-    #filter the OSes
-    for path in os_list:
-        #print("os path in operation: \t", path)
-        filtered = list(filter(None, re.split(' > ' , path)))
+    # filter the OSes
+    for path in os_list :
+        # print("os path in operation: \t", path)
+        filtered = list(filter(None , re.split(' > ' , path)))
         filtered_word_sensitive = re.sub('[' + string.punctuation + ']' , '> ' , path).split()
-        while ">" in filtered:
+        while ">" in filtered :
             filtered == filtered.remove(">")
-        while ">" in filtered_word_sensitive:
+        while ">" in filtered_word_sensitive :
             filtered_word_sensitive == filtered_word_sensitive.remove(">")
-        #print("keywords:\t\t ", filtered)
-        #print("word sensitive:\t\t ", filtered_word_sensitive)
-        #print("inputed keywords:\t ", filtered_input)
+        # print("keywords:\t\t ", filtered)
+        # print("word sensitive:\t\t ", filtered_word_sensitive)
+        # print("inputed keywords:\t ", filtered_input)
 
-        #compare the values
+        # compare the values
         all_filtered = removeduplicate(filtered_word_sensitive + filtered)
-        #print("----------------------------------------------")
-        #print("success_treshold = " , success_treshold)
-        for item in all_filtered:
-            for keyword in filtered_input:
-                matchval = round(matcher(None , keyword.lower() , item.lower()).ratio(), 3)
-                #if matchval!=0:
-                    #print(keyword , "to" , item, "\t--->", round(matchval*100, 3),"%")
-                #else:
-                    #print(keyword, "to", item, "\tNO MATCH")
-                if matchval >= success_treshold:
-                    passed_filter.append(path)
-        #print("\n\n")
+        # print("----------------------------------------------")
+        # print("success_treshold = " , success_treshold)
+        for item in all_filtered :
+            for keyword in filtered_input :
+                matchval = round(matcher(None , keyword.lower() , item.lower()).ratio() , 3)
+                # if matchval!=0:print(keyword , "to" , item, "\t--->", round(matchval*100, 3),"%")
+                # else:print(keyword, "to", item, "\tNO MATCH")
+                if matchval >= success_treshold : passed_filter.append(path)
+        # print("\n\n")
 
-    return list(removeduplicate(passed_filter))
+    # finalize
+    out = list(removeduplicate(passed_filter))
+    if len(out) != 0 :
+        info , success = str("{} results found in {} seconds".format(str(len(out)) , str(round(float(time.time() - start_time) , 3)))) , True
+    else :
+        info , success = "No search results found" , False
+    return success , out , info
 
-def clear():
-    if platform.system() == "Linux" :
-        system("clear")
-    if platform.system() == "Windows" :
-        system("cls")
+def clear() :
+    if platform.system() == "Linux" : system("clear")
+    if platform.system() == "Windows" : system("cls")
 
-if __name__ == "__main__":
-    clear()
-    repo, meta = initialize()
-    all_oses_list = index(repo)
-    user = input("> ")
-    user == user.lower()
-    if user.startswith("search "):
-        result = search(all_oses_list, user)
-        for i in result:
-            print(i)
-    elif user == "list":
-        for os in all_oses_list:
+def interface() :
+    print("""
+OSRepo:
+
+The only repo you'll need to download any kind of OSes.
+Type 'help' to see a list of commands
+
+Enter a command to continue:
+""")
+
+def help() :
+    print("""
+list
+  //lists all OSes currently in the repo
+  
+download
+  //where you can download the OSes
+  
+search <args>
+  //you can search for any OS or group here!
+    search <args> (multiple args can be used)  
+help
+  //a list of commands available
+  
+open
+  //allows the user to read the repo in yaml formal
+  
+meta
+  //goes to meta menu where you can see more info about the current repo
+  
+update (windows only)
+  //installs the latest version of OSR if a new version is available
+  
+uninstall (windows only)
+  //uninstall OSR
+    """)
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+def get_command(user) :
+    if user.startswith("search ") :
+        if remove_prefix(user, search).replace(" ", ""):
+            success, search_result, info = search(all_oses_list, user)
+    elif user == "list" :
+        for os in all_oses_list :
             print(os)
-    elif user == "explore":
+    elif user == "download" :
         clear()
         walk(repo)
-    elif user == "tag":
+    elif user == "help" :
         clear()
-        for path in all_oses_list:
-            tag(meta, all_oses_list)
+        help()
+    elif user == "open" :
+        clear()
+        system("start " + temp("osrepo.yaml"))
+        main()
+
+def main() :
+    clear()
+    interface()
+    get_command(input("> ").lower())
+
+if __name__ == "__main__" :
+    clear()
+    repo , meta = initialize()
+    all_oses_list = index(repo)
+    main()
